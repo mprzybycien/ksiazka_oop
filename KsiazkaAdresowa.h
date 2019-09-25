e9e3a632b67a67223f1f3ceca1bb1bde281ec0e1
Mateusz Przybycień <mateusz.przybycien.programista@gmail.com>
2019-09-25 15:50:04 +0200

    Created base class KsiazkaAdresowa and Uzytkownik

---
+++ KsiazkaAdresowa.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include "Uzytkownik.h"
+
+using namespace std;
+
+class KsiazkaArdesowa
+{
+    int idZalogowanegoUzytkownika;
+    int idOstatniegoAdresata;
+    int idUsunietegoAdresata;
+
+    vector <Uzytkownik> uzytkownicy;
+
+public:
+
+};
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "KsiazkaAdresowa.h"
+
+using namespace std;
+
+int main()
+{
+    KsiazkaArdesowa ksiazkaAdresowa;
+}
